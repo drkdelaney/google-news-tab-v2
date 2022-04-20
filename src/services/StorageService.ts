945d37fc10438bb02cb3becec5e900b3a170a0e2
Derek Delaney <derek.e.delaney@gmail.com>
2022-04-20 15:49:05 -0700

    update version

--- src/services/StorageService.ts
+++ src/services/StorageService.ts
@@ -2,28 +2,31 @@ import { Topic } from '../models';
 const STORAGE_KEY = 'topics';
 
 export function saveTopics(topics: Topic[]): void {
-    // setStorage(STORAGE_KEY, topics);
+  // Comment out to run locally
+  setStorage(STORAGE_KEY, topics);
 }
 
 export async function loadTopics(): Promise<Topic[]> {
-    // return getStorage<Topic[]>(STORAGE_KEY);
-    return new Promise((resolve, reject) => {resolve([])});
+  // Comment out to run locally
+  return getStorage<Topic[]>(STORAGE_KEY);
+  // Uncomment to run locally
+  // return new Promise((resolve, reject) => {resolve([])});
 }
 
 function setStorage(key: string, value: any): void {
-    chrome.storage.sync.set({ [key]: value });
+  chrome.storage.sync.set({ [key]: value });
 }
 
 function getStorage<T>(key: string): Promise<T> {
-    return new Promise<T>((resolve, reject) => {
-        chrome.storage.sync.get([key], (result: any) => {
-            if (chrome.runtime.lastError) {
-                reject(chrome.runtime.lastError.message);
-            } else if (key in result) {
-                resolve(result[key]);
-            } else {
-                reject(`${key} not found in result: ${result}`);
-            }
-        });
+  return new Promise<T>((resolve, reject) => {
+    chrome.storage.sync.get([key], (result: any) => {
+      if (chrome.runtime.lastError) {
+        reject(chrome.runtime.lastError.message);
+      } else if (key in result) {
+        resolve(result[key]);
+      } else {
+        reject(`${key} not found in result: ${result}`);
+      }
     });
+  });
 }
